e95a1181c7244c1846a1295b04e75a3d914a7820
kshreve <kshreve@gmail.com>
2012-01-30 16:39:59 -0500

    Update github.php

--- github.php
+++ github.php
@@ -1,1 +1,5 @@
-<?php `C:\cygwin\bin\bash -li /cygdrive/c/Users/Administrator/asdf.sh`;?>
+<?php
+echo 'hello <br />';
+`C:\cygwin\bin\bash -li /cygdrive/c/Users/Administrator/asdf.sh`;
+echo 'goodbye'
+?>
