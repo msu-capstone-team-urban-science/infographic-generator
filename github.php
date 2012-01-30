25229cd6b4bb282109d608fa181a2f60969b93e5
kshreve <kshreve@gmail.com>
2012-01-30 17:07:41 -0500

    Update github.php

--- github.php
+++ github.php
@@ -1,5 +1,5 @@
 <?php
-$output = shell_exec("C:\cygwin\bin\bash -li /cygdrive/c/Users/Administrator/asdf.sh");
+$output = shell_exec("C:\\cygwin\\bin\\bash -li /cygdrive/c/Users/Administrator/asdf.sh");
 echo "<pre>$output</pre>";
 print_r(shell_exec('dir'));
 echo 'done';
