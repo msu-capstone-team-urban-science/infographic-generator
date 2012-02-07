a8c63fe239a1250fd9a901e45e7d363fde770852
Kevin Shreve <kshreve@gmail.com>
2012-02-07 13:29:10 -0500

    d

--- github.php
+++ github.php
@@ -1,7 +1,15 @@
 <?php
+echo "<html>";
+echo "<title>github.php</title>";
+echo "<body>";
+
 `cd /cygdrive/c/inetpub/wwwroot/infographic-generator/`;
 echo "<pre> git pull</pre>"
 `git pull`; 
 echo "<pre>post pull</pre>"
-echo 'done';
+echo 'done<br/>';
+
+echo "</body>";
+echo "</html>;
+
 ?>
