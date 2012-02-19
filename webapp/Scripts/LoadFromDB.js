5a7b25dbb581050a4bd66761dc9b7000c0aff567
U-WIN-RGEKHF6IJ8B\Administrator <Administrator@WIN-RGEKHF6IJ8B.(none)>
2012-02-19 14:46:05 -0500

    Added folders and changed all the paths to make them work

--- webapp/Scripts/LoadFromDB.js
+++ webapp/Scripts/LoadFromDB.js
@@ -5,7 +5,7 @@
 
 $(document).ready(function () {
     var store = new Storage();
-    $.getJSON("../KPI_Handler.ashx", function (item) {
+    $.getJSON("KPI_Handler.ashx", function (item) {
         $.each(item, function (i, kpi) {
             //TODO: check if the create date is newer than the stored create 
             store.set(kpi.record_number, kpi);
