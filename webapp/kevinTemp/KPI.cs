69b53b56b538ed70d9faf7753127fdd79e7462d1
Kevin Shreve <kshreve@gmail.com>
2012-02-18 21:27:29 -0500

    Committing all of the server stuff to test.
    Signed-off-by: Kevin Shreve <kshreve@gmail.com>

---
+++ webapp/kevinTemp/KPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/*************************
+*Created by: Kevin Shreve*
+*Date : 2/18/12          *
+*************************/
+
+//This is the default class for KPI, containing all the columns that match with our database.
+public class KPI
+{
+    public KPI()
+    {
+        //Do not use in prod
+    }
+	public KPI(int record_numberI, string kpi_name, DateTime kpi_date, double kpi_value, DateTime create_dtI )
+	{
+        record_number = record_numberI;
+        KPI_Name = kpi_name;
+        KPI_Date = kpi_date;
+        KPI_Value = kpi_value;
+        create_dt = create_dtI;
+	}
+
+    public DateTime Date() {
+        return DateTime.Now;
+    }
+ 
+    public int record_number { get; set; }
+    public string KPI_Name { get; set; }
+    public DateTime KPI_Date { get; set; }
+    public double KPI_Value { get; set; }
+    public DateTime create_dt { get; set; }
+}
