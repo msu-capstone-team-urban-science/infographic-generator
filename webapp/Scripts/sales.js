869ff29eddf97b508ec95121087569316ad4e06c
Kevin Shreve <kshreve@gmail.com>
2012-02-26 20:15:20 -0500

    fix the clear functino again

--- webapp/Scripts/sales.js
+++ webapp/Scripts/sales.js
@@ -22,7 +22,15 @@ function init(date) {
 function clearCanvas(c) {
     var canvas = document.getElementById(c);
 	var ctx = canvas.getContext("2d");
+	// Store the current transformation matrix
+	ctx.save();
+
+	// Use the identity matrix while clearing the canvas
+	ctx.setTransform(1, 0, 0, 1, 0, 0);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	// Restore the transform
+	ctx.restore();
 }
 
 function drawGraph01(date) {
