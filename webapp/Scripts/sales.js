f3ff9baaaaf357362ad79744a1582831d5c21fbe
Kevin Shreve <kshreve@gmail.com>
2012-02-26 20:01:54 -0500

    trying a new clear function to clear the canvas'

--- webapp/Scripts/sales.js
+++ webapp/Scripts/sales.js
@@ -21,10 +21,16 @@ function init(date) {
 
 function clearCanvas(c) {
     var canvas = document.getElementById(c);
-    var context = canvas.getContext("2d");
-    context.fillStyle = "#FFF";	
-    context.rect(0, 0, canvas.width, canvas.height);
-    context.fill();
+	var context = canvas.getContext("2d");
+	// Store the current transformation matrix
+	ctx.save();
+
+	// Use the identity matrix while clearing the canvas
+	ctx.setTransform(1, 0, 0, 1, 0, 0);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	// Restore the transform
+	ctx.restore();
 }
 
 function drawGraph01(date) {
