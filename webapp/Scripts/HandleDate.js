806719ba7517e31e10a3bd0f14b94f00a5104e67
Kevin Shreve <kshreve@gmail.com>
2012-03-13 16:04:59 -0400

    can't get anything to work.

--- webapp/Scripts/HandleDate.js
+++ webapp/Scripts/HandleDate.js
@@ -1,23 +1,15 @@
 var monthname = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
 var fullMonthName = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-var showing_date_now;
+var currDate;
 
 function initDate() {
-	if(!window.showing_date_now) {
-		window.showing_date_now = new Date;
-		alert(window.showing_date_now);
-		initSales(window.showing_date_now);
-		GetFullMonth(window.showing_date_now);
+	if(!currDate) {
+		currDate = new Date;
+		initSales(currDate);
+		GetFullMonth(currDate);
 	}
 }
 
-function getCurrDate() { 
-	return window.showing_date_now;
-}
-
-function setCurrDate(d) {
-	window.showing_date_now = d;
-}
 
 function GetFullMonth(todate) {
 	document.getElementById('month').innerHTML = fullMonthName[todate.getMonth()];
