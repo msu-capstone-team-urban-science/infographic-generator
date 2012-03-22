d40722f934908e7742679f962ab4e9851ad0c5a9
Louis Bodnar <pezmanlou@gmail.com>
2012-03-21 23:43:33 -0400

    fixed percents

--- webapp/Scripts/draw-new-sales.js
+++ webapp/Scripts/draw-new-sales.js
@@ -207,8 +207,8 @@
                 context.restore();
 
 
-                DrawPie("myCanvas", 150, 1050, 100, 100, parseInt(GetKPI(date, "Dealer_Retention"))/100);
-                DrawPie("myCanvas", 750, 1050, 100, 100, parseInt(GetKPI(date, "Visits_Per_Customer"))/100);
+                DrawPie("myCanvas", 150, 1050, 100, 100, parseInt(GetKPI(date, "Dealer_Retention")));
+                DrawPie("myCanvas", 750, 1050, 100, 100, parseInt(GetKPI(date, "Visits_Per_Customer")));
 
 
             }
