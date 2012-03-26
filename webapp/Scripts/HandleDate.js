602c3e9e8e12650297c68b466cf0c3adf1e38103
Kevin Shreve <kshreve@gmail.com>
2012-03-25 20:10:35 -0400

    tried to make a unicode space

--- webapp/Scripts/HandleDate.js
+++ webapp/Scripts/HandleDate.js
@@ -11,9 +11,9 @@ function initDate() {
 }
 function GetFullMonth(todate,b) {
 
-    document.getElementById('month').innerHTML = fullMonthName[todate.getMonth()] +"'"+ todate.getFullYear().toString().substr(2,3);
+    document.getElementById('month').innerHTML = fullMonthName[todate.getMonth()] +" '"+ todate.getFullYear().toString().substr(2,3);
     if (b) {
-        document.getElementById('month2').innerHTML = fullMonthName[todate.getMonth()-b] +"'"+ todate.getFullYear().toString().substr(2,3);
+        document.getElementById('month2').innerHTML = fullMonthName[todate.getMonth()-b] +" '"+ todate.getFullYear().toString().substr(2,3);
     }
 }
 
