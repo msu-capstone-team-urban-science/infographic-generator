1a976b66364b0df796fcd0b8e34d4a05c9a098cc
Louis Bodnar <pezmanlou@gmail.com>
2012-03-28 18:08:56 -0400

    reconfubled the energy-mo-tron

--- webapp/Scripts/elements.js
+++ webapp/Scripts/elements.js
@@ -656,10 +656,10 @@ function DrawCompetitiveSegmentSale(c,x,y,w,h,Comp_Seg_Sale_data) {
 
         for (var i = 0; i < Comp_Seg_Sale_data.length; i++)
         {
-            // draw dealership name
+            // draw dealership name (works)
             ctx6.fillText(Comp_Seg_Sale_data[i][1], x, y + img03.height + i * (img03.height + lineSpacing));
 
-            // calculate bar width
+            // calculate bar width (broken)
             var barWidth = (parseInt(Comp_Seg_Sale_data[i][0])/parseInt(Comp_Seg_Sale_data[0][0]))*(650 - textWidth - img03.width);
 
             // draw bar
