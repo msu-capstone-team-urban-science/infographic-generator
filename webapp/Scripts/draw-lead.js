d78e7a53073193353d33b6bfb06169b9b1a73a31
Lok <lokarithm@gmail.com>
2012-04-01 18:46:03 -0400

    working on Lead.html
    Signed-off-by: Lok <lokarithm@gmail.com>

--- webapp/Scripts/draw-lead.js
+++ webapp/Scripts/draw-lead.js
@@ -299,3 +299,35 @@ function DrawLead6(c,date) {
 	context.restore();
 }
 
+$(document).ready(function () {
+	// if user clicked on button, the overlay layer or the dialogbox, close the dialog	
+	$('#dialog-overlay, #dialog-box').bind("touchstart click", function () {		
+		$('#dialog-overlay, #dialog-box').hide();		
+		return false;
+	});
+	
+	// if user resize the window, call the same function again
+	// to make sure the overlay fills the screen and dialogbox aligned to center	
+	$(window).resize(function () {
+		
+		//only do it if the dialog box is not hidden
+		if (!$('#dialog-box').is(':hidden')) popup();		
+	});		
+});
+
+function popup(message) {
+	// get the screen height and width  
+	var maskHeight = $(document).height();  
+	var maskWidth = $(window).width();
+	
+	// calculate the values for center alignment
+	var dialogTop =  (maskHeight/3) - ($('#dialog-box').height());  
+	var dialogLeft = (maskWidth/2) - ($('#dialog-box').width()/2); 
+	
+	// assign values to the overlay and dialog box
+	$('#dialog-overlay').css({height:maskHeight, width:maskWidth}).show();
+	$('#dialog-box').css({top:dialogTop, left:dialogLeft}).show();
+	
+	// display the message
+	$('#dialog-message').html(message);	
+}
