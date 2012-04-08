f9eac1de215fe3cec873733829a1f5eb0da32d5a
Lok Cheung <lokarithm@gmail.com>
2012-04-08 08:16:40 -0400

    added insertion query for data
    Signed-off-by: Lok Cheung <lokarithm@gmail.com>

---
+++ customer-resources/database_insertion_query/dataInsert_Apr12.sql
@@ -0,0 +1,229 @@
+          
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Retail_Sales','March 2012',166,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Used_Vehicle_Sales','March 2012',55,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES('Dealer_Retention','March 2012',0.301,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES('Visits_Per_Customer','March 2012',0.569,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Labor_Ops_Per_RO','March 2012',2.9,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Active_Customers','March 2012',1451,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Inactive_Customers','March 2012',1954,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Single_Visit_Customers','March 2012',561,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Recent_Sales_Customers','March 2012',191,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Service_Labor_Opportunity','March 2012',238120,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Service_Parts_Opportunity','March 2012',176209,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('RO_Count','March 2012',1815,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Average_Money_Per_RO','March 2012',134.84,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Prospect_Count','March 2012',1294,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Dealer_Effectiveness','March 2012',0.819,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Brand_Effectiveness','March 2012',1.156,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Lost_Profit','March 2012',823216,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Lost_Sales','March 2012',417,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Pump_In_Sales_Anytown_Automotive','March 2012',653,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Pump_In_Sale_Allan_Automart','March 2012',365,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Pump_In_Sale_Jefferson_Automotive','March 2012',324,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Pump_In_Sale_Nestor_Auto_Center','March 2012',30,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Pump_In_Sale_Diamond_Automotive','March 2012',28,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Pump_In_Sale_Anthony_Motors','March 2012', 21,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Competitive_Segment_Sale_Anytown_Automotive','March 2012',81,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Competitive_Segment_Sale_Jeff_Williams_Toyotas','March 2012',152,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Competitive_Segment_Sale_Uptown_Honda','March 2012', 131,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Competitive_Segment_Sale_Fred_Rodgers_Mazda','March 2012',142,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Competitive_Segment_Sale_Garrett_Ford','March 2012',57,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Competitive_Segment_Sale_Peter_Lake_Ford','March 2012',40,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('New_Brand_Leads','March 2012',1501,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('New_3PL_Leads_Kelly_Blue_Book','March 2012',331,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('New_3PL_Leads_Edmunds','March 2012',269,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('New_3PL_Leads_Dealix','March 2012',108,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('New_3PL_Leads_Automotive.com','March 2012',36,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('New_3PL_Leads_Jumpstart','March 2012',11,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Unique_Customers','March 2012',1805,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Average_Response_Time','March 2012',50.2,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Close_Rate','March 2012',0.026,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Lost_Sales_From_Leads','March 2012',9,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('New_Sales_From_Leads','March 2012',35,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Used_Sales_From_Leads','March 2012',12,GETDATE())
+           
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES
+           ('Cost_Per_Sale','March 2012',195,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES('Unopened_Leads','March 2012',203,GETDATE())           
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES('Response_Method_Phone','March 2012',677,GETDATE())
+
+INSERT INTO [urbanScience].[dbo].[KPI_data]
+           ([KPI_Name],[KPI_Date],[KPI_Value],[create_dt])
+     VALUES('Response_Method_Email','March 2012',1579,GETDATE())
+
+GO
+
+
